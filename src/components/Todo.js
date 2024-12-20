229d6cc602fb35ab5d038f266747c77013137178
Délanno Kotcho <delanktc24@gmail.com>
2024-12-20 10:29:02 +0100

    finition du todo list app

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,13 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const Todo = ({task}) => {
+export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
   return (
     <div className='Todo'>
-        <p>{task.task}</p> 
+        <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed': ''}`}>{task.task}</p> 
         <div>
-        <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} />
-        <FontAwesomeIcon className="delete-icon" icon={faTrash} />
+        <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)}/>
+        <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
         </div>
     </div>
   )
